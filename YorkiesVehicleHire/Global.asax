f82f7ffc8b5276bd3e9d389cb154b07dc44b12b0
Faisal Khan <faisalalam98@gmail.com>
2018-04-11 21:33:35 +0100

    Add project files.

---
+++ YorkiesVehicleHire/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YorkiesVehicleHire.MvcApplication" Language="C#" %>
---
+++ YorkiesVehicleHire/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YorkiesVehicleHire.Startup))]
+namespace YorkiesVehicleHire
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
