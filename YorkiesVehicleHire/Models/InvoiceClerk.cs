7135ace1c046e3beb1199243d80862d5f95161d9
Faisal Khan <faisalalam98@gmail.com>
2018-04-12 23:06:08 +0100

    Invoice Clerk subclass model

---
+++ YorkiesVehicleHire/Models/InvoiceClerk.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YorkiesVehicleHire.Models
+{
+    public class InvoiceClerk : Person  //Subclass of Person
+    {
+        //Display attribute specifies what to display for the name of the field
+        //Required attribute specifies that a field is required
+
+        public int InvoiceClerkID { get; set; }
+
+        //UserID variable with DisplayAttribute and RequiredAttribute
+        [Display(Name = "User ID"), Required]
+        public int UserID { get; set; }
+
+        public InvoiceClerk() { }
+    }
+}
