92c91d6526450efb4a23a4ae957f4b8d1353f624
Faisal Khan <faisalalam98@gmail.com>
2018-04-12 21:42:17 +0100

    Booking Clerk subclass model

---
+++ YorkiesVehicleHire/Models/BookingClerk.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YorkiesVehicleHire.Models
+{
+    public class BookingClerk : Person  //Subclass of Person
+    {
+        //Display attribute specifies what to display for the name of the field
+        //Required attribute specifies that a field is required
+
+        public int BookingClerkID { get; set; }
+
+        //UserID variable with DisplayAttribute and RequiredAttribute
+        [Display(Name = "User ID"), Required]
+        public int UserID { get; set; }
+
+    }
+}
