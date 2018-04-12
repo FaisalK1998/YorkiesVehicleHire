b1b8d450607ba99e7e0a39a086cc4f20a8eb7016
Faisal Khan <faisalalam98@gmail.com>
2018-04-12 21:49:31 +0100

    Manager subclass model

---
+++ YorkiesVehicleHire/Models/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YorkiesVehicleHire.Models
+{
+    public class Manager : Person   //Subclass of Person
+    {
+        //Display attribute specifies what to display for the name of the field
+        //Required attribute specifies that a field is required
+
+        public int ManagerID { get; set; }
+
+        //UserID variable with DisplayAttribute and RequiredAttribute
+        [Display(Name = "User ID"), Required]
+        public int UserID { get; set; }
+
+        public Manager() { }
+    }
+}
