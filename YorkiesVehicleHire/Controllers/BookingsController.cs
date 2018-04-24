8e19df87ac67bb4cbdf0ebf223440410062bb955
Faisal Khan <faisalalam98@gmail.com>
2018-04-24 22:19:54 +0100

    Added Controllers and made changes to layout

---
+++ YorkiesVehicleHire/Controllers/BookingsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YorkiesVehicleHire.Controllers
+{
+    public class BookingsController : Controller
+    {
+        // GET: Bookings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Bookings/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Bookings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bookings/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bookings/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Bookings/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bookings/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
