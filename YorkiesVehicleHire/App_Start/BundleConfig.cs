47b622612462be0211b722a2871eb02b50a22848
Faisal Khan <faisalalam98@gmail.com>
2018-04-24 18:44:57 +0100

    Added Bootstrap package and Bootswatch theme

--- YorkiesVehicleHire/App_Start/BundleConfig.cs
+++ YorkiesVehicleHire/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace YorkiesVehicleHire
+
 {
     public class BundleConfig
     {
@@ -13,19 +13,24 @@ namespace YorkiesVehicleHire
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
+            
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
+
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
+            
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-simplex.css",
                       "~/Content/site.css"));
+
         }
+
     }
+
 }
